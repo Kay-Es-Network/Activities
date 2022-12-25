79f2e0fd4a88acbaf46469517454990484edf76b
AEndrix <38986519+AEndrix03@users.noreply.github.com>
2022-12-25 04:42:52 +0100

    Add Database login via File

--- src/it/aendrix/activities/Main.java
+++ src/it/aendrix/activities/Main.java
@@ -1,7 +1,12 @@
 package it.aendrix.activities;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.yaml.snakeyaml.Yaml;
 
+import java.io.File;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.HashMap;
 
@@ -15,12 +20,12 @@ public class Main extends JavaPlugin {
         instance = this;
 
         try {
-            database = new Database("localhost", 3306, "Activities", "password", "user", "activity");
+            database();
 
-            this.players = this.database.selectAllToday();
+            players = database.selectAllToday();
 
             Main.getInstance().getServer().getPluginManager().registerEvents(new Listener(), this);
-        } catch (SQLException | ClassNotFoundException e) {
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
 
@@ -29,6 +34,43 @@ public class Main extends JavaPlugin {
     public void onDisable() {
         for (PlayerInstance player : players.values())
             player.stop();
+    }
+
+    private void database() {
+        new File("plugins/Activities").mkdir();
+
+        File f = new File("plugins/Activities" + File.separator + "database.yml");
+        FileConfiguration cfg = YamlConfiguration.loadConfiguration(f);
+        if (!f.exists())
+            try {
+                f.createNewFile();
+
+                cfg.set("database.address", "localhost");
+                cfg.set("database.port", 3306);
+                cfg.set("database.user", "Activities");
+                cfg.set("database.password", "password");
+                cfg.set("database.database", "Activities");
+                cfg.set("database.table", "Activity");
+
+                cfg.save(f);
+            } catch (IOException ignored) {}
+        else
+            try {
+                String address = cfg.getString("database.localhost"),
+                db = cfg.getString("database.database"),
+                password = cfg.getString("database.password"),
+                user = cfg.getString("database.user"),
+                table = cfg.getString("database.table");
+
+                database = new Database(
+                        address != null ? address : "localhost",
+                        cfg.getInt("database.port"),
+                        db != null ? db : "Activities",
+                        password != null ? password : "password",
+                        user != null ? user : "Activities",
+                        table != null ? table : "Activity"
+                );
+            } catch (SQLException | ClassNotFoundException ignored) {}
     }
 
     public static Main getInstance() {
